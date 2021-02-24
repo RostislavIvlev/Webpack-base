73d52adc11497ccfbca92d60ef17ed7d9ade890c
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2021-02-24 17:25:01 +0500

    Обновление безопасности Небольшие изменения в блоках и стилях

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-// Autoadd all scss/css/ts files
 import "../node_modules/modern-normalize/modern-normalize.css"
+
+// Autoadd all scss/css/ts files
 
 function importAll (r) {
     r.keys().forEach(r);
